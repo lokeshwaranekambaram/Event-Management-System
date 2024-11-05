20f18425fa0826643af2550908c9fd1b7e27817f
lokeshwaranekambaram <lokesh22905@gmail.com>
2024-11-05 14:44:17 +0530

    feat : new feature

--- Event-Management-System/src/main/java/com/system/eventmanagement/eventmanagementsystem/model/Book.java
+++
@@ -1,5 +0,0 @@
-package com.system.eventmanagement.eventmanagementsystem.model;
-
-public class Book {
-
-}
--- Event-Management-System/src/main/java/com/system/eventmanagement/eventmanagementsystem/model/Events.java
+++ Event-Management-System/src/main/java/com/system/eventmanagement/eventmanagementsystem/model/Events.java
@@ -1,5 +1,19 @@
 package com.system.eventmanagement.eventmanagementsystem.model;
 
+import java.sql.Time;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Events {
+     @Id
+     private int id;
+     private String eventName;
+     private String eventDescription;
+     private String eventplace;
+     private Time eventTime;
+     private Date eventDate;
+
 
 }
